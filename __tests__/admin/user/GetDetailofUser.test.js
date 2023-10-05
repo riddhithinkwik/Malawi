7d2e8dd9a5dfd6433a1a58d8fffe4377cab9e252
riddhithinkwik <riddhi.p@thinkwik.com>
2023-10-05 13:51:36 +0530

    Testcases For Get User Details API

---
+++ __tests__/admin/user/GetDetailofUser.test.js
@@ -0,0 +1,74 @@
+const axios = require("axios");
+const FormData = require("form-data");
+const { serialize } = require("object-to-formdata");
+var qs = require("qs");
+var data = qs.stringify({});
+const { login } = require("../../../helpers/utils.helper");
+let userId = "if6QCabSUY3k9dafsUrvlVaO";
+
+describe("GET /api/v1/admin/user", () => {
+  const exec = async () => {
+    return new Promise(async (resolve) => {
+      try {
+        const formData = new FormData();
+
+        const res = await axios({
+          method: "get",
+          url: `${process.env.BASE_PATH}/admin/user/${userId}`,
+          headers: {
+            "x-mei-auth-token": accessToken,
+            "x-client-id": "someid",
+            //...data.getHeaders(),
+          },
+          data: data,
+        });
+        return resolve(res.data);
+      } catch (e) {
+        if (e.isAxiosError) {
+          return resolve(e.response.data);
+        }
+        return resolve(e);
+      }
+    });
+  };
+
+  it("should return 200 with code `SUCCESS` if the user data has been received properly", async () => {
+    const {
+      useraccessToken: accessTokenOrg,
+      userRefreshToken: refreshTokenOrg,
+    } = await login();
+    refreshToken = refreshTokenOrg;
+    accessToken = accessTokenOrg;
+    const response = await exec();
+    expect(response.statusCode).toBe(200);
+    expect(response.status).toBe("SUCCESS");
+  });
+
+  it("should return 400 with code `INVALID_USER` if userId is not valid ", async () => {
+    const {
+      useraccessToken: accessTokenOrg,
+      userRefreshToken: refreshTokenOrg,
+    } = await login();
+    refreshToken = refreshTokenOrg;
+    accessToken = accessTokenOrg;
+    userId = "dd";
+    const response = await exec();
+
+    expect(response.statusCode).toBe(400);
+    expect(response.status).toBe("INVALID_USER");
+  });
+
+  it("should return 401 with code `UNAUTHORIZED` if Unauthorized to use this content ", async () => {
+    const {
+      useraccessToken: accessTokenOrg,
+      userRefreshToken: refreshTokenOrg,
+    } = await login();
+    refreshToken = refreshTokenOrg;
+    accessToken = "sqsq";
+
+    const response = await exec();
+
+    expect(response.statusCode).toBe(401);
+    expect(response.status).toBe("UNAUTHORIZED");
+  });
+});
